f661625e7cd6e600903025e4d3a38eb7be0343f0
hblancolo <hector.classicsound@gmail.com>
2016-10-08 16:51:21 +0200

    Acabado el punto 4
    Comprobar lo de la apertura y cierre de ficheros correcta

--- karaoke.py
+++ karaoke.py
@@ -5,6 +5,7 @@ import sys
 from smallsmilhandler import SmallSMILHandler
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
+import json
 
 
 class SSHv2(SmallSMILHandler):
@@ -17,15 +18,30 @@ class SSHv2(SmallSMILHandler):
 
             print(linea)
 
-try:
-    fichero_leido = open(sys.argv[1])
-except:
-    sys.exit("Usage: python3 karaoke.py file.smil")
+    def unir_etiqs_attrs(self):  # junto attrs y etiquetas en dicc_datos
+        self.dicc_datos = {}
+        for i in range(len(self.etiquetas)):
+            self.dicc_datos[self.etiquetas[i]] = self.attrs[i]
 
-with fichero_leido as fichero:
+    def smil_to_json(self):
+        name_fich = sys.argv[1].split('.')[0]
+        self.fich_json = open(name_fich + '.json', 'w')
+        codigo_json = json.dumps(self.dicc_datos)
+        self.fich_json.write(codigo_json)
+
+if __name__ == "__main__":
+    try:
+        fichero_leido = open(sys.argv[1])
+    except:
+        sys.exit("Usage: python3 karaoke.py file.smil")
 
     parser = make_parser()
     cHandler = SSHv2()
     parser.setContentHandler(cHandler)
-    parser.parse(fichero)
+    with fichero_leido as fichero:
+        parser.parse(fichero)
+
     cHandler.get_tags_v2()
+    cHandler.unir_etiqs_attrs()
+    cHandler.smil_to_json()
+    cHandler.fich_json.close()
