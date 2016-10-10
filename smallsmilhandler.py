c12f2e89786d63eb101d26434345a560ee7ef16b
hblancolo <hector.classicsound@gmail.com>
2016-10-10 16:21:17 +0200

    Finalizado y PEP8. Cambiada estructura de datos

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -9,41 +9,34 @@ class SmallSMILHandler(ContentHandler):
 
     def __init__(self):
 
-        self.etiquetas = []  # lista de nombres de etiqueta, empiezan en [1]
-        self.attrs = []  # lista de diccionarios de atributos asociados
-
-    def startElement(self, name, attrs):
-
+        self.datos = []  # lista de listas. cada sublista tiene dos elementos:
+# el primero es el nombre de etiqueta, el segundo el dicc de atributos
         attrs_rootlayout = ['width', 'height', 'background-color']
         attrs_region = ['id', 'top', 'bottom', 'left', 'right']
         attrs_img = ['src', 'region', 'begin', 'dur']
         attrs_audio = ['src', 'begin', 'dur']
         attrs_textstream = ['src', 'region', 'fill']
-        dicc_etiquetas = {'root-layout': attrs_rootlayout,
-                          'region': attrs_region, 'img': attrs_img,
-                          'audio': attrs_audio, 'textstream': attrs_textstream}
+        self.dicc_etiquetas = {'root-layout': attrs_rootlayout,
+                               'region': attrs_region, 'img': attrs_img,
+                               'audio': attrs_audio,
+                               'textstream': attrs_textstream}
 
-        if (name == 'root-layout' or name == 'region' or name == 'img'
-           or name == 'audio' or name == 'textstream'):
-            self.etiquetas.append(name)
-            # diccionario de los atributos de la etiqueta en la que me
-            # encuentro actualmente. Luego lo añado a la lista de diccionarios
+    def startElement(self, name, attrs):
+
+        if name in self.dicc_etiquetas:
             dicc = {}
-            for attr in dicc_etiquetas[name]:
-                if attrs.get(attr, "") != "":  # excluyo atributos vacios
-                    dicc[attr] = attrs.get(attr, "")
+            for attr in self.dicc_etiquetas[name]:
+                dicc[attr] = attrs.get(attr, "")
 
-            self.attrs.append(dicc)
+            self.datos.append([name, dicc])
 
     def get_tags(self):
 
-        for i in range(len(self.etiquetas)):
-            print(self.etiquetas[i] + ' ' + str(self.attrs[i]))
-            print()
+        return self.datos
 
 if __name__ == "__main__":
     parser = make_parser()
     cHandler = SmallSMILHandler()
     parser.setContentHandler(cHandler)
     parser.parse(open('karaoke.smil'))
-    cHandler.get_tags()
+    print(cHandler.get_tags())
