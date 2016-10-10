0ee7ff2794895686e7e7b6f33a5ad496982ca44d
hblancolo <hector.classicsound@gmail.com>
2016-10-10 16:22:10 +0200

    Finalizado y PEP8. Adaptado a nueva est. de datos

--- karaoke.py
+++ karaoke.py
@@ -19,19 +19,15 @@ class KaraokeLocal(SmallSMILHandler):
 
     def __str__(self):
         self.str_lineas = ''
-        for i in range(len(self.etiquetas)):
-            linea = self.etiquetas[i]
-            for atributo, valor in self.attrs[i].items():
-                linea = linea + '\t' + atributo + '=' + '"' + valor + '"'
+        for i in range(len(self.datos)):
+            linea = self.datos[i][0]  # coge el nombre de etiqueta
+            for atributo, valor in self.datos[i][1].items():
+                if valor != "":
+                    linea = linea + '\t' + atributo + '=' + '"' + valor + '"'
 
             self.str_lineas = self.str_lineas + linea + '\n'
 
         print(self.str_lineas)
-
-    def unir_etiqs_attrs(self):  # junto attrs y etiquetas en dicc_datos
-        self.dicc_datos = {}
-        for i in range(len(self.etiquetas)):
-            self.dicc_datos[self.etiquetas[i]] = self.attrs[i]
 
     def to_json(self, json_name):
         self.name_fich = sys.argv[1]
@@ -41,21 +37,20 @@ class KaraokeLocal(SmallSMILHandler):
             self.name_json = json_name
 
         fich_json = open(self.name_json, 'w')
-        codigo_json = json.dumps(self.dicc_datos)
+        codigo_json = json.dumps(self.datos)
         fich_json.write(codigo_json)
+        fich_json.close()
 
     def do_local(self):
         self.lista_remotos = []
-        for dicc in self.attrs:
-            for atributo in dicc:
-                if dicc[atributo][:7] == 'http://':
-# esto guarda direccion recurso, y modifica valor de atributo en attrs por
-# el nombre del recurso en si (ej.: smile.jpg)
-                    self.lista_remotos.append(dicc[atributo])
-                    dicc[atributo] = dicc[atributo].split('/')[-1]
+        for i in range(len(self.datos)):
+            for atributo, valor in self.datos[i][1].items():
+                if valor[:7] == 'http://':
+                    self.lista_remotos.append(valor)
+                    self.datos[i][1][atributo] = valor.split('/')[-1]
         for remoto in self.lista_remotos:
             name_fich_local = remoto.split('/')[-1]
-            urlretrieve(remoto, "/home/hector/PTAVI/P3/" + name_fich_local)
+            urlretrieve(remoto, name_fich_local)
 
 if __name__ == "__main__":
     try:
@@ -66,7 +61,7 @@ if __name__ == "__main__":
     kar_loc = KaraokeLocal()
     kar_loc.inicializador(fichero_leido, kar_loc)
     kar_loc.__str__()
-    kar_loc.unir_etiqs_attrs()
     kar_loc.to_json('')  # como parametro se pasa el nombre del fichero json
     kar_loc.do_local()
     kar_loc.to_json('local.json')
+    kar_loc.__str__()
